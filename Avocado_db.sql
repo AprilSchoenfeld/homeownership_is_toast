ee9ddabf2432373825efb49e6e401460be05947e
April Schoenfeld <45743915+AprilSchoenfeld@users.noreply.github.com>
2019-04-13 11:06:50 -0400

    Add files via upload

---
+++ Avocado_db.sql
@@ -0,0 +1,17 @@
+Create table avocado(
+id Int Auto_increment,
+start_date Date,
+AveragePrice Float,
+TotalVolume Float,
+`4046` Float,	
+`4225` Float,	
+`4770`Float,
+TotalBags Float,	
+SmallBags Float,	
+LargeBags Float,	
+XLargeBags Float,
+`type` Text,	
+`Year` Year,	
+ region Text,
+primary Key (id)
+);
